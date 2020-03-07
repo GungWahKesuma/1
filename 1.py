53094548b6dbe725ecd15028e4917c82c4f824a4
GungWahKesuma <32163815+GungWahKesuma@users.noreply.github.com>
2020-03-07 21:14:49 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+iguana = 2169
+days = 276
+daycount = 92
+death = (11.1/100)
+temp = iguana
+day = 0
+
+
+while(days>day):
+    temp = temp - round(temp*death)
+    temp = temp+temp
+    day = day+daycount
+    print("total iguana (day %d) is = %d" % (day, temp))
+
+print("count : ",temp)
